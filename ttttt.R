2223cb3e6f4210958df0fecba2b771f5b4d5e36b
GABASKI <98767071+GABASKI@users.noreply.github.com>
2023-12-08 22:10:06 +0200

    hbd 2

--- ttttt.R
+++ ttttt.R
@@ -1,48 +1,48 @@
 ####   READING THE DATA #######
- #Load the data
-news <- read.csv("/home/kali/InferPro/news.csv" , )
+# Load the data
+news <- read.csv("news.csv")
 printsep <- function() {
   print("-----------------------------------------------------------------------------------------------")
 }
 
 ####     Understanding the structure of the  data  ####
 
- #look at columns names
+# look at columns names
 names(news)
 printsep()
- # view the whole data 
+# view the whole data
 View(news)
 printsep()
- #useful summary of dataset structure
+# useful summary of dataset structure
 str(news)
 printsep()
 #######    DATA CLEANING ##########
-  # 1-Remove negative values from X column 
-news$X[news$X<0]<-news$X[news$X<0]*(-1)
-  # 2-Omit null values 
+# 1-Remove negative values from X column
+news$X[news$X < 0] <- news$X[news$X < 0] * (-1)
+# 2-Omit null values
 printsep()
-print('Before Clearing Nulls:')
-print(paste('Number Of Rows Contain Nulls: ', nrow(news[rowSums(is.na(news)) > 0, ])))
-print('...........................................................')
-news<-na.omit(news)
-print('After Clearing Nulls:')
-print(paste('Number Of Rows Contain Nulls: ', nrow(news[rowSums(is.na(news)) > 0, ])))
+print("Before Clearing Nulls:")
+print(paste("Number Of Rows Contain Nulls: ", nrow(news[rowSums(is.na(news)) > 0, ])))
+print("...........................................................")
+news <- na.omit(news)
+print("After Clearing Nulls:")
+print(paste("Number Of Rows Contain Nulls: ", nrow(news[rowSums(is.na(news)) > 0, ])))
 printsep()
- # 3-check duplicates 
-print(paste('Number Of Duplicated Rows: ', nrow(news[duplicated(news),])))
+# 3-check duplicates
+print(paste("Number Of Duplicated Rows: ", nrow(news[duplicated(news), ])))
 printsep()
 
- # 4- Modify Label column 
-news$label[news$label=="F"]<- "FAKE"
-news$label[news$label=="R"]<- "REAL"
+# 4- Modify Label column
+news$label[news$label == "F"] <- "FAKE"
+news$label[news$label == "R"] <- "REAL"
 View(news)
- 
+
 ########   Visualizing #######
-#summary(news)
-#plot(news$text, news$label, xlab = "text", ylab = "label",  main = "Type of Diet Effect on Weight", col = "blue")
-#plot(news[-c(1000)])
-#hist(news$text, col = "lightblue", xlab = "text", main = "text histo ")
-x = as.data.frame(news)
+# summary(news)
+# plot(news$text, news$label, xlab = "text", ylab = "label",  main = "Type of Diet Effect on Weight", col = "blue")
+# plot(news[-c(1000)])
+# hist(news$text, col = "lightblue", xlab = "text", main = "text histo ")
+x <- as.data.frame(news)
 
 
 ########  Using classification methods ########
